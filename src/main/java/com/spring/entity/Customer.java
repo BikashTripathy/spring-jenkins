026bef35f2844619c97a11e69126a40ccb206f94
BikashTripathy <bikash.t.info@gmail.com>
2023-02-13 16:57:12 +0530

    initital commit

---
+++ src/main/java/com/spring/entity/Customer.java
@@ -0,0 +1,64 @@
+package com.spring.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+
+	private int id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String Address;
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(String firstName, String lastName, String address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		Address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", Address=" + Address
+				+ "]";
+	}
+
+}
